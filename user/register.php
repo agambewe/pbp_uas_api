18490030b2aac97bf3f4ae33ce826c49918ada20
= <=>
2019-11-26 22:54:13 +0700

    update

--- user/register.php
+++ user/register.php
@@ -28,13 +28,13 @@ if(isset($_POST['name'])){
         $mail->SMTPDebug = 2;  
         $mail->IsSMTP();        //Sets Mailer to send message using SMTP
         $mail->Host = 'mail.drugsative.xyz';  //Sets the SMTP hosts of your Email hosting, this for Godaddy
-        $mail->Port = 587;        //Sets the default SMTP server port
+        $mail->Port = 465;        //Sets the default SMTP server port
         $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
         // $mail->Username = 'arekkaos@gmail.com';     //Sets SMTP username
         // $mail->Password = 'Arek_Kaos!#';     //Sets SMTP password
         $mail->Username = 'drugsati_agam';
         $mail->Password = 'PAW_ICONIC!';
-        $mail->SMTPSecure = 'tls';       //Sets connection prefix. Options are "", "ssl" or "tls"
+        $mail->SMTPSecure = 'ssl';       //Sets connection prefix. Options are "", "ssl" or "tls"
         $mail->From = 'staff@arekkaos.com';   //Sets the From email address for the message
         $mail->FromName = $toko;     //Sets the From name of the message
         $mail->AddAddress($email, $name);  //Adds a "To" address   
