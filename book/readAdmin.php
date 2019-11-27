08d5fe6912eb0ee4e316a2de1a3c5a8418433bca
= <=>
2019-11-27 11:55:18 +0700

    update

---
+++ book/readAdmin.php
@@ -0,0 +1,13 @@
+<?php
+include('../db.php');
+
+$query = mysqli_query($con,"SELECT * FROM books") or die(mysqli_error($con));
+$array_data = array();
+
+while($data = mysqli_fetch_assoc($query)){
+    $array_data['code'] = 1;
+    $array_data['data'][] = $data;
+}
+
+echo json_encode($array_data);
+?>
