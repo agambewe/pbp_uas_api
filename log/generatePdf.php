c89fa2093b08bd80a72a626f5637008479f8f508
= <=>
2019-11-27 08:40:17 +0700

    update

--- log/generatePdf.php
+++ log/generatePdf.php
@@ -18,18 +18,18 @@ $pdf->Cell(10,7,'',0,1);
 
 $pdf->SetFont('Arial','B',12);
 $pdf->Cell(10,6,'NO',1,0);
-$pdf->Cell(40,6,'EMAIL',1,0);
-$pdf->Cell(25,6,'SEAT',1,0);
-$pdf->Cell(55,6,'BOOK',1,0);
+$pdf->Cell(55,6,'EMAIL',1,0);
+$pdf->Cell(55,6,'SEAT',1,0);
+$pdf->Cell(55,6,'BOOK',1,1);
 
 $pdf->SetFont('Arial','',12);
 $query = mysqli_query($con, "SELECT * FROM logs");
 $no = 1;
 while ($data = mysqli_fetch_array($query)){
     $pdf->Cell(10,6,$no,1,0);
-    $pdf->Cell(40,6,$data['email'],1,0);
-    $pdf->Cell(25,6,$data['seat'],1,0);
-    $pdf->Cell(55,6,$data['currenBook'],1,0);
+    $pdf->Cell(55,6,$data['email'],1,0);
+    $pdf->Cell(55,6,$data['seat'],1,0);
+    $pdf->Cell(55,6,$data['currentBook'],1,0);
     $no++;
 }
 
